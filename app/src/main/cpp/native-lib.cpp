a1496ecbe64ac670964d49132d81a971b0628572
Li Yang <liyangzmx@163.com>
2020-07-31 18:12:07 +0800

    First Edition.
    Signed-off-by: Li Yang <liyangzmx@163.com>

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -10,6 +10,8 @@ extern "C" {
 #include <libavformat/avformat.h>
 #include <libswscale/swscale.h>
 #include <libavutil/imgutils.h>
+#include <libswresample/swresample.h>
+#include <libavutil/opt.h>
 
 // version.h
 #include <libavcodec/version.h>
@@ -68,6 +70,12 @@ Java_com_wyze_ffmpegnative_MainActivity_setSurface(JNIEnv *env, jobject thiz, jo
         LOGE("Couldn't open file:%s\n", file_name);
         return -1;
     }
+    jclass thisClass = env->GetObjectClass(thiz);
+    jfieldID gpuId = env->GetFieldID(thisClass, "gpuImageView", "Ljp/co/cyberagent/android/gpuimage/GPUImageView;");
+    jobject gpuObject = env->GetObjectField(thiz, gpuId);
+    jclass gpuClass = env->GetObjectClass(gpuObject);
+    jmethodID pJmethodId = env->GetMethodID(gpuClass, "updatePreviewFrame", "([BII)V");
+
     if(avformat_find_stream_info(pFormatCtx, NULL)<0) {
         LOGE("Couldn't find stream information.");
         return -1;
@@ -123,10 +131,21 @@ Java_com_wyze_ffmpegnative_MainActivity_setSurface(JNIEnv *env, jobject thiz, jo
         return -1;
     }
 
+    AVFrame * pFrameYUV420p = av_frame_alloc();
+    if(pFrameYUV420p == NULL) {
+        LOGE("Could not allocate pFrameYUV420p frame.");
+        return -1;
+    }
+
     // Determine required buffer size and allocate buffer
     int numBytes=av_image_get_buffer_size(AV_PIX_FMT_RGBA, pCodecCtx->width, pCodecCtx->height, 1);
-    uint8_t * buffer=(uint8_t *)av_malloc(numBytes*sizeof(uint8_t));
+    uint8_t * buffer=(uint8_t *)av_malloc(numBytes * sizeof(uint8_t));
     av_image_fill_arrays(pFrameRGBA->data, pFrameRGBA->linesize, buffer, AV_PIX_FMT_RGBA,
+                         pCodecCtx->width, pCodecCtx->height, 1);
+
+    int yuv_numBytes=av_image_get_buffer_size(AV_PIX_FMT_NV21, pCodecCtx->width, pCodecCtx->height, 1);
+    uint8_t * yuv_buffer=(uint8_t *)av_malloc(yuv_numBytes * sizeof(uint8_t));
+    av_image_fill_arrays(pFrameYUV420p->data, pFrameYUV420p->linesize, yuv_buffer, AV_PIX_FMT_NV21,
                          pCodecCtx->width, pCodecCtx->height, 1);
     LOGE("NICK %s: %d", __func__, __LINE__);
     // 由于解码出来的帧格式不是RGBA的,在渲染之前需要进行格式转换
@@ -141,9 +160,21 @@ Java_com_wyze_ffmpegnative_MainActivity_setSurface(JNIEnv *env, jobject thiz, jo
                                                 NULL,
                                                 NULL);
 
+    struct SwsContext *sws_ctx_yuv = sws_getContext(pCodecCtx->width,
+                                                pCodecCtx->height,
+                                                pCodecCtx->pix_fmt,
+                                                pCodecCtx->width,
+                                                pCodecCtx->height,
+                                                AV_PIX_FMT_NV21,
+                                                    SWS_BILINEAR,
+                                                NULL,
+                                                NULL,
+                                                NULL);
+
     int frameFinished;
     AVPacket packet;
     LOGE("NICK %s: %d", __func__, __LINE__);
+    jbyteArray yuv_array = env->NewByteArray(yuv_numBytes);
     while(av_read_frame(pFormatCtx, &packet)>=0) {
         int ret = 0;
         if(packet.stream_index==videoStream) {
@@ -164,34 +195,44 @@ Java_com_wyze_ffmpegnative_MainActivity_setSurface(JNIEnv *env, jobject thiz, jo
                     return ret;
                 }
 
-                // write the frame data to output file
                 ANativeWindow_lock(nativeWindow, &windowBuffer, 0);
-
-                // 格式转换
                 sws_scale(sws_ctx, (uint8_t const * const *)pFrame->data,
-                          pFrame->linesize, 0, pCodecCtx->height,
+                          pFrame->linesize, 0, pFrame->height,
                           pFrameRGBA->data, pFrameRGBA->linesize);
-
-                // 获取stride
                 uint8_t * dst = (uint8_t *)windowBuffer.bits;
                 int dstStride = windowBuffer.stride * 4;
                 uint8_t * src = (uint8_t*) (pFrameRGBA->data[0]);
                 int srcStride = pFrameRGBA->linesize[0];
-                // 由于window的stride和帧的stride不同,因此需要逐行复制
                 int h;
                 for (h = 0; h < videoHeight; h++) {
                     memcpy(dst + h * dstStride, src + h * srcStride, srcStride);
                 }
                 ANativeWindow_unlockAndPost(nativeWindow);
-                av_frame_unref(pFrame);
-                if (ret < 0)
+
+                ret = sws_scale(sws_ctx_yuv, (uint8_t const * const *)pFrame->data,
+                          pFrame->linesize, 0, pFrame->height,
+                          pFrameYUV420p->data, pFrameYUV420p->linesize);
+                if (ret < 0) {
                     return ret;
+                }
+                av_image_copy_to_buffer(yuv_buffer, yuv_numBytes,
+                                        pFrameYUV420p->data,
+                                        pFrameYUV420p->linesize, AV_PIX_FMT_NV21,
+                                        pFrameYUV420p->width, pFrameYUV420p->height, 1);
+                env->SetByteArrayRegion(yuv_array, 0, yuv_numBytes,
+                                        (const jbyte *)yuv_buffer);
+                env->CallVoidMethod(gpuObject, pJmethodId, yuv_array, pCodecCtx->width, pCodecCtx->height);
+
+                av_frame_unref(pFrame);
             }
         }
         av_packet_unref(&packet);
     }
+    env->DeleteLocalRef(yuv_array);
     av_free(buffer);
+    av_free(yuv_buffer);
     av_free(pFrameRGBA);
+    av_free(pFrameYUV420p);
 
     // Free the YUV frame
     av_free(pFrame);
@@ -203,6 +244,3 @@ Java_com_wyze_ffmpegnative_MainActivity_setSurface(JNIEnv *env, jobject thiz, jo
     avformat_close_input(&pFormatCtx);
     return 0;
 }
-
-
-
--- app/src/main/java/com/wyze/ffmpegnative/MainActivity.java
+++ app/src/main/java/com/wyze/ffmpegnative/MainActivity.java
@@ -2,18 +2,55 @@ package com.wyze.ffmpegnative;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.ImageFormat;
+import android.graphics.Rect;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.CaptureResult;
+import android.hardware.camera2.params.OutputConfiguration;
+import android.hardware.camera2.params.SessionConfiguration;
+import android.media.Image;
+import android.media.ImageReader;
+import android.opengl.GLSurfaceView;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
+import android.util.Log;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.TextView;
 
+import java.lang.reflect.Array;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import jp.co.cyberagent.android.gpuimage.GPUImage;
+import jp.co.cyberagent.android.gpuimage.GPUImageView;
+import jp.co.cyberagent.android.gpuimage.filter.GPUImageGrayscaleFilter;
+import jp.co.cyberagent.android.gpuimage.filter.GPUImageSketchFilter;
+import jp.co.cyberagent.android.gpuimage.filter.GPUImageSmoothToonFilter;
+import jp.co.cyberagent.android.gpuimage.util.Rotation;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static boolean VERBOSE = false;
+    private static String TAG= "NICK: ";
+
     // Used to load the 'native-lib' library on application startup.
+
     static {
         System.loadLibrary("avutil");
         System.loadLibrary("swresample");
@@ -27,27 +64,234 @@ public class MainActivity extends AppCompatActivity {
     }
     HandlerThread mHandlerThread = new HandlerThread("Task");
     Handler mBackHandler;
+    GPUImage gpuImage;
+    GLSurfaceView glSurfaceView;
+    GPUImageView gpuImageView;
+    CameraDevice mCameraDevice;
+    String cameraId = null;
+    ImageReader imageReader;
+    CaptureRequest.Builder requestBuilder;
+    CameraManager cameraManager;
+    private static final int COLOR_FormatI420 = 1;
+    private static final int COLOR_FormatNV21 = 2;
+
+    private static boolean isImageFormatSupported(Image image) {
+        int format = image.getFormat();
+        switch (format) {
+            case ImageFormat.YUV_420_888:
+            case ImageFormat.NV21:
+            case ImageFormat.YV12:
+                return true;
+        }
+        return false;
+    }
+
+    private static byte[] getDataFromImage(Image image, int colorFormat) {
+        if (colorFormat != COLOR_FormatI420 && colorFormat != COLOR_FormatNV21) {
+            throw new IllegalArgumentException("only support COLOR_FormatI420 " + "and COLOR_FormatNV21");
+        }
+        if (!isImageFormatSupported(image)) {
+            throw new RuntimeException("can't convert Image to byte array, format " + image.getFormat());
+        }
+        Rect crop = image.getCropRect();
+        int format = image.getFormat();
+        int width = crop.width();
+        int height = crop.height();
+        Image.Plane[] planes = image.getPlanes();
+        byte[] data = new byte[width * height * ImageFormat.getBitsPerPixel(format) / 8];
+        byte[] rowData = new byte[planes[0].getRowStride()];
+        if (VERBOSE) Log.v(TAG, "get data from " + planes.length + " planes");
+        int channelOffset = 0;
+        int outputStride = 1;
+        for (int i = 0; i < planes.length; i++) {
+            switch (i) {
+                case 0:
+                    channelOffset = 0;
+                    outputStride = 1;
+                    break;
+                case 1:
+                    if (colorFormat == COLOR_FormatI420) {
+                        channelOffset = width * height;
+                        outputStride = 1;
+                    } else if (colorFormat == COLOR_FormatNV21) {
+                        channelOffset = width * height + 1;
+                        outputStride = 2;
+                    }
+                    break;
+                case 2:
+                    if (colorFormat == COLOR_FormatI420) {
+                        channelOffset = (int) (width * height * 1.25);
+                        outputStride = 1;
+                    } else if (colorFormat == COLOR_FormatNV21) {
+                        channelOffset = width * height;
+                        outputStride = 2;
+                    }
+                    break;
+            }
+            ByteBuffer buffer = planes[i].getBuffer();
+            int rowStride = planes[i].getRowStride();
+            int pixelStride = planes[i].getPixelStride();
+            if (VERBOSE) {
+                Log.v(TAG, "pixelStride " + pixelStride);
+                Log.v(TAG, "rowStride " + rowStride);
+                Log.v(TAG, "width " + width);
+                Log.v(TAG, "height " + height);
+                Log.v(TAG, "buffer size " + buffer.remaining());
+            }
+            int shift = (i == 0) ? 0 : 1;
+            int w = width >> shift;
+            int h = height >> shift;
+            buffer.position(rowStride * (crop.top >> shift) + pixelStride * (crop.left >> shift));
+            for (int row = 0; row < h; row++) {
+                int length;
+                if (pixelStride == 1 && outputStride == 1) {
+                    length = w;
+                    buffer.get(data, channelOffset, length);
+                    channelOffset += length;
+                } else {
+                    length = (w - 1) * pixelStride + 1;
+                    buffer.get(rowData, 0, length);
+                    for (int col = 0; col < w; col++) {
+                        data[channelOffset] = rowData[col * pixelStride];
+                        channelOffset += outputStride;
+                    }
+                }
+                if (row < h - 1) {
+                    buffer.position(buffer.position() + rowStride - length);
+                }
+            }
+            if (VERBOSE) Log.v(TAG, "Finished reading data from plane " + i);
+        }
+        return data;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
         setContentView(R.layout.activity_main);
         mHandlerThread.start();
         mBackHandler = new Handler(mHandlerThread.getLooper());
 
-        // Example of a call to a native method
+        gpuImage = new GPUImage(this);
+//        glSurfaceView = findViewById(R.id.gpu_view);
+        gpuImageView = findViewById(R.id.gpu_view);
+        gpuImageView.setFilter(new GPUImageSketchFilter());
+        gpuImageView.setRotation(Rotation.NORMAL);
+        gpuImageView.setRenderMode(GPUImageView.RENDERMODE_CONTINUOUSLY);
+
+//        gpuImage.setFilter(new GPUImageSketchFilter());
+//        gpuImage.setGLSurfaceView(glSurfaceView);
+        int img_width = 1920;
+        int img_height = 1080;
+
+//        imageReader = ImageReader.newInstance(img_width, img_height, ImageFormat.YUV_420_888, 5);
+//        imageReader.setOnImageAvailableListener(new ImageReader.OnImageAvailableListener() {
+//            @Override
+//            public void onImageAvailable(ImageReader imageReader) {
+//                if(imageReader != null){
+//                    Image image = imageReader.acquireNextImage();
+//                    if(image != null) {
+////                        gpuImage.updatePreviewFrame(getDataFromImage(image, COLOR_FormatNV21), image.getWidth(), image.getHeight());
+//                        gpuImageView.updatePreviewFrame(getDataFromImage(image, COLOR_FormatNV21), image.getWidth(), image.getHeight());
+//                        image.close();
+//                    }
+//                }
+//            }
+//        }, null);
+//
+////        final Surface previewSurfaec = surfaceHolder.getSurface();
+//        try {
+//            String[] cameraList = cameraManager.getCameraIdList();
+//            for (String camera : cameraList) {
+//                CameraCharacteristics cameraCharacteristics = cameraManager.getCameraCharacteristics(camera);
+//                Integer cameraFace = cameraCharacteristics.get(CameraCharacteristics.LENS_FACING);
+//                if (cameraFace == null) {
+//                    return;
+//                }
+//                if (cameraFace == CameraCharacteristics.LENS_FACING_FRONT) {
+//                    cameraId = camera;
+//                }
+//            }
+//        } catch (CameraAccessException e) {
+//            e.printStackTrace();
+//        }
+//        SurfaceView tv = findViewById(R.id.sample_text);
+//        SurfaceHolder surfaceHolder = tv.getHolder();
+//        surfaceHolder.addCallback(new SurfaceHolder.Callback() {
+//            @Override
+//            public void surfaceCreated(@NonNull final SurfaceHolder surfaceHolder) {
+//                final Surface surface = surfaceHolder.getSurface();
+//                try {
+//                    cameraManager.openCamera(cameraId,
+//                            new CameraDevice.StateCallback() {
+//                                @Override
+//                                public void onOpened(@NonNull CameraDevice cameraDevice) {
+//                                    mCameraDevice = cameraDevice;
+//                                    try {
+//                                        requestBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+//                                        requestBuilder.addTarget(surfaceHolder.getSurface());
+//                                        requestBuilder.addTarget(imageReader.getSurface());
+//                                        mCameraDevice.createCaptureSession(Arrays.asList(surfaceHolder.getSurface(), imageReader.getSurface()),
+//                                                new CameraCaptureSession.StateCallback() {
+//                                            @Override
+//                                            public void onConfigured(@NonNull CameraCaptureSession cameraCaptureSession) {
+//                                                try {
+//                                                    cameraCaptureSession.setRepeatingRequest(requestBuilder.build(), null
+//                                                            , null);
+//                                                } catch (CameraAccessException e) {
+//                                                    e.printStackTrace();
+//                                                }
+//                                            }
+//
+//                                            @Override
+//                                            public void onConfigureFailed(@NonNull CameraCaptureSession cameraCaptureSession) {
+//
+//                                            }
+//                                        }, null);
+//                                    } catch (CameraAccessException e) {
+//                                        e.printStackTrace();
+//                                    }
+//                                }
+//
+//                                @Override
+//                                public void onDisconnected(@NonNull CameraDevice cameraDevice) {
+//
+//                                }
+//
+//                                @Override
+//                                public void onError(@NonNull CameraDevice cameraDevice, int i) {
+//
+//                                }
+//                            }, mBackHandler);
+//                } catch (CameraAccessException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//
+//            @Override
+//            public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+//
+//            }
+//
+//            @Override
+//            public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+//
+//            }
+//        });
+
         SurfaceView tv = findViewById(R.id.sample_text);
         SurfaceHolder surfaceHolder = tv.getHolder();
         surfaceHolder.addCallback(new SurfaceHolder.Callback() {
             @Override
-            public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
-                final Surface surface = surfaceHolder.getSurface();
-                mBackHandler.post(new Runnable() {
+            public void surfaceCreated(@NonNull final SurfaceHolder surfaceHolder) {
+                new AsyncTask<Void, Integer, Integer>() {
                     @Override
-                    public void run() {
-                        setSurface(surface);
+                    protected Integer doInBackground(Void... voids) {
+                        setSurface(surfaceHolder.getSurface());
+                        return 0;
                     }
-                });
+                }.execute();
             }
 
             @Override
